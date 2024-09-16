9f7dd6844ac3b2f7e5240af6a86c642321b4ae4b
Dave Flynn <analogue40@gmail.com>
2024-09-16 21:26:30 +0800

    initial commit

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+// Import our custom CSS
+import '../scss/styles.sass'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+import 'bootstrap-icons/font/bootstrap-icons.scss'
+
+
+// You can also import JavaScript plugins individually as needed to keep bundle sizes down:
+//import Alert from 'bootstrap/js/dist/alert';
+
+// or, specify which plugins you need:
+//import { Tooltip, Toast, Popover } from 'bootstrap';
